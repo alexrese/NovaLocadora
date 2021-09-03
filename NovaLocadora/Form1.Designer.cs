46951c5b4d034c4f096458cc70eb3e0a9284ae0f
Alex Luciano Roesler Rese <alexrese@outlook.com>
2021-09-02 22:01:35 -0300

    Data grid, banco e inserir

---
+++ NovaLocadora/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿
+namespace NovaLocadora
+{
+    partial class FormLocadora
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgLocadora = new System.Windows.Forms.DataGridView();
+            this.colID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colCategoria = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colDescricao = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colAno = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbNome = new System.Windows.Forms.TextBox();
+            this.tbCategoria = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbDescricao = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbAno = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btLimpar = new System.Windows.Forms.Button();
+            this.btInserir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgLocadora)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgLocadora
+            // 
+            this.dgLocadora.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgLocadora.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colID,
+            this.colName,
+            this.colCategoria,
+            this.colDescricao,
+            this.colAno});
+            this.dgLocadora.Location = new System.Drawing.Point(12, 199);
+            this.dgLocadora.Name = "dgLocadora";
+            this.dgLocadora.Size = new System.Drawing.Size(764, 199);
+            this.dgLocadora.TabIndex = 0;
+            // 
+            // colID
+            // 
+            this.colID.HeaderText = "ID";
+            this.colID.Name = "colID";
+            this.colID.Width = 30;
+            // 
+            // colName
+            // 
+            this.colName.HeaderText = "Name";
+            this.colName.Name = "colName";
+            this.colName.Width = 150;
+            // 
+            // colCategoria
+            // 
+            this.colCategoria.HeaderText = "Categoria";
+            this.colCategoria.Name = "colCategoria";
+            // 
+            // colDescricao
+            // 
+            this.colDescricao.HeaderText = "Descrição";
+            this.colDescricao.Name = "colDescricao";
+            this.colDescricao.Width = 350;
+            // 
+            // colAno
+            // 
+            this.colAno.HeaderText = "Ano";
+            this.colAno.Name = "colAno";
+            this.colAno.Width = 50;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nome";
+            // 
+            // tbNome
+            // 
+            this.tbNome.Location = new System.Drawing.Point(60, 20);
+            this.tbNome.Name = "tbNome";
+            this.tbNome.Size = new System.Drawing.Size(100, 20);
+            this.tbNome.TabIndex = 2;
+            // 
+            // tbCategoria
+            // 
+            this.tbCategoria.Location = new System.Drawing.Point(233, 20);
+            this.tbCategoria.Name = "tbCategoria";
+            this.tbCategoria.Size = new System.Drawing.Size(100, 20);
+            this.tbCategoria.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(175, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Categoria";
+            // 
+            // tbDescricao
+            // 
+            this.tbDescricao.Location = new System.Drawing.Point(403, 20);
+            this.tbDescricao.Name = "tbDescricao";
+            this.tbDescricao.Size = new System.Drawing.Size(100, 20);
+            this.tbDescricao.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(342, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Descrição";
+            // 
+            // tbAno
+            // 
+            this.tbAno.Location = new System.Drawing.Point(560, 20);
+            this.tbAno.Name = "tbAno";
+            this.tbAno.Size = new System.Drawing.Size(100, 20);
+            this.tbAno.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(528, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Ano";
+            // 
+            // btLimpar
+            // 
+            this.btLimpar.Location = new System.Drawing.Point(52, 63);
+            this.btLimpar.Name = "btLimpar";
+            this.btLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btLimpar.TabIndex = 9;
+            this.btLimpar.Text = "Limpar";
+            this.btLimpar.UseVisualStyleBackColor = true;
+            this.btLimpar.Click += new System.EventHandler(this.btLimpar_Click);
+            // 
+            // btInserir
+            // 
+            this.btInserir.Location = new System.Drawing.Point(152, 63);
+            this.btInserir.Name = "btInserir";
+            this.btInserir.Size = new System.Drawing.Size(75, 23);
+            this.btInserir.TabIndex = 10;
+            this.btInserir.Text = "Inserir";
+            this.btInserir.UseVisualStyleBackColor = true;
+            this.btInserir.Click += new System.EventHandler(this.btInserir_Click);
+            // 
+            // FormLocadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btInserir);
+            this.Controls.Add(this.btLimpar);
+            this.Controls.Add(this.tbAno);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbDescricao);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbCategoria);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbNome);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgLocadora);
+            this.Name = "FormLocadora";
+            this.Text = "Locadora";
+            this.Load += new System.EventHandler(this.FormLocadora_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgLocadora)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgLocadora;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colCategoria;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDescricao;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colAno;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbNome;
+        private System.Windows.Forms.TextBox tbCategoria;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbDescricao;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbAno;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btLimpar;
+        private System.Windows.Forms.Button btInserir;
+    }
+}
+
---
+++ NovaLocadora/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace NovaLocadora
+{
+    public partial class FormLocadora : Form
+    {
+        public FormLocadora()
+        {
+            InitializeComponent();
+        }
+
+        private MySqlConnectionStringBuilder conexaoBanco()
+        {
+            MySqlConnectionStringBuilder conexaoBD = new MySqlConnectionStringBuilder();
+            conexaoBD.Server = "localhost";
+            conexaoBD.Database = "locadora";
+            conexaoBD.UserID = "root";
+            conexaoBD.Password = "";
+            conexaoBD.SslMode = 0;
+            return conexaoBD;
+        }
+
+        private void FormLocadora_Load(object sender, EventArgs e)
+        {
+            atualizaGrid();
+        }
+
+        private void atualizaGrid()
+        {
+            MySqlConnectionStringBuilder conexaoBD = conexaoBanco();
+            MySqlConnection realizaConexacoBD = new MySqlConnection(conexaoBD.ToString());
+            try
+            {
+                realizaConexacoBD.Open();
+
+                MySqlCommand comandoMySql = realizaConexacoBD.CreateCommand();
+                comandoMySql.CommandText = "SELECT * FROM filme WHERE ativoFilme = 1";
+                MySqlDataReader reader = comandoMySql.ExecuteReader();
+
+                dgLocadora.Rows.Clear();
+
+                while (reader.Read())
+                {
+                    DataGridViewRow row = (DataGridViewRow)dgLocadora.Rows[0].Clone();//FAZ UM CAST E CLONA A LINHA DA TABELA
+                    row.Cells[0].Value = reader.GetInt32(0);//ID
+                    row.Cells[1].Value = reader.GetString(1);//NOME
+                    row.Cells[2].Value = reader.GetString(2);//DESCRICAO
+                    row.Cells[3].Value = reader.GetString(3);//CATEGORIA
+                    row.Cells[4].Value = reader.GetString(4);//ANO
+                    dgLocadora.Rows.Add(row);//ADICIONO A LINHA NA TABELA
+                }
+
+                realizaConexacoBD.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can not open connection ! ");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void btLimpar_Click(object sender, EventArgs e)
+        {
+            limparCampos();
+        }
+
+        private void limparCampos()
+        {
+            tbAno.Clear();
+            tbNome.Clear();
+            tbCategoria.Clear();
+            tbDescricao.Clear();
+        }
+
+        private void btInserir_Click(object sender, EventArgs e)
+        {
+            MySqlConnectionStringBuilder conexaoBD = conexaoBanco();
+            MySqlConnection realizaConexacoBD = new MySqlConnection(conexaoBD.ToString());
+            try
+            {
+                realizaConexacoBD.Open();
+
+                MySqlCommand comandoMySql = realizaConexacoBD.CreateCommand();
+             
+                comandoMySql.CommandText = "INSERT INTO filme (nomeFilme,categoriaFilme,descricaoFilme,anoFilme) " +
+                    "VALUES('" + tbNome.Text + "', '" + tbCategoria.Text + "','" + tbDescricao.Text + "', " + Convert.ToInt16(tbAno.Text) + ")";
+                comandoMySql.ExecuteNonQuery();
+
+                realizaConexacoBD.Close();
+                MessageBox.Show("Inserido com sucesso");
+                atualizaGrid();
+                limparCampos();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
